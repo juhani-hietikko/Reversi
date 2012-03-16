36fd37479f2ad42e61b9033aedf1e61f584d9267
Juhani Hietikko <juhani.hietikko@houston-inc.com>
2012-03-16 20:16:09 +0200

    A static html version of the initial gameboard

--- client/app/test/board_tests.js
+++ client/app/test/board_tests.js
@@ -1,14 +1,47 @@
 steal("funcunit").then(function() {
 
-	module("example",{
+	module("board tests",{
 		setup: function() {
 			S.open('http://localhost:8080/app/index.html');
 		}
 	});
 	
-	test("says hello",function() {
-		S('p').visible(function() {
-			equal( S('p').text(), "Hello, World!");
+	test("displays title", function() {
+		S(document).ready(function() {
+			equal(S('h1').text(), "Reversi");
+		});
+	});
+	
+	test("displays a 8x8 board", function() {
+		S(document).ready(function() {
+			equal(S('table').length, 1);
+			equal(S('table').find('tr').length, 8);
+			equal(S('table').find('td').length, 64);
+		});
+	});
+	
+	test("sets up the initial disc pattern on the board", function() {
+		
+		var WHITE_DISC = "<img src=\"img/white.png\">";
+		var BLACK_DISC = "<img src=\"img/black.png\">";
+		var EMPTY_SQUARE = "<img src=\"img/empty.png\">";
+		
+		S(document).ready(function() {
+			S('table tr').each(function(rowIndex, row) {
+				S(row).find('td').each(function(colIndex, cell) {
+					if (rowIndex == 3 && colIndex == 3) {
+						equal(cell.innerHTML, WHITE_DISC);		
+					} else if (rowIndex == 3 && colIndex == 4) {
+						equal(cell.innerHTML, BLACK_DISC);		
+					} else if (rowIndex == 4 && colIndex == 3) {
+						equal(cell.innerHTML, BLACK_DISC);		
+					} else if (rowIndex == 4 && colIndex == 4) {
+						equal(cell.innerHTML, WHITE_DISC);
+					} else {
+						equal(cell.innerHTML, EMPTY_SQUARE);
+					}
+				});
+			});
 		});
 	});
 });
